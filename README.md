7111c59d45371efc0c0ddef2fb5eb0d44fd8a713
newton <chiakajunior@yahoo.com>
2021-04-17 02:53:51 +0100

    assesement complete, my first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+to start the project use Npm init.
+
+in this assessement, i used node-fetch to get json data from an api, and wrote the json data to a file called posts.json in the result directory.
---
+++ index.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch");
+const fs = require('fs');
+
+const url = 'http://jsonplaceholder.typicode.com/posts';
+
+fetch(url)
+.then((resp) => resp.json())
+.then((data) => {
+    fs.writeFile('result/posts.json',JSON.stringify(data,null, 2), function (err) {
+        if (err) throw err;
+        console.log('Updated!');
+      });
+})
+.catch(error => console.log(error));
